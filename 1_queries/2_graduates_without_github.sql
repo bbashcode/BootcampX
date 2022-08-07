d7f5ee9bf2132c1aaaedce23cc779418a0a69aae
bbashcode <binbash.code@gmail.com>
2022-08-07 07:45:32 +0000

    Set up the JavaScript project

--- 1_queries/2_graduates_without_github.sql
+++ 1_queries/2_graduates_without_github.sql
@@ -2,3 +2,19 @@ SELECT name, email, phone
 FROM students
 WHERE github IS NULL
 AND end_date < NOW();
+
+CREATE TABLE assignment(
+id SERIAL PRIMARY KEY INT NOT NULL,
+name varchar(255) NOT NULL,
+content varchar(255),
+day date,
+chapter varchar(255),
+duration varchar(40)
+);
+
+CREATE TABLE assignment_submissions(
+id SERIAL PRIMARY KEY INT NOT NULL,
+assignment_id INT REFERENCES assignment(id) ON DELETE CASCADE,
+student_id INT REFERENCES students(id) ON DELETE CASCADE,
+
+);
